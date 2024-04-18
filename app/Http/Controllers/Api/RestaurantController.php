5a4d4e5469634a46a3531f2732bcb7d5ccbc0404
MaximeNlr <158460952+MaximeNlr@users.noreply.github.com>
2024-04-18 09:51:17 +0200

    update restaurant

--- app/Http/Controllers/Api/RestaurantController.php
+++ app/Http/Controllers/Api/RestaurantController.php
@@ -14,8 +14,8 @@ class RestaurantController extends Controller
      */
     public function index()
     {
-        // récupérer et retourner tous les restaurants:
-        $restaurants = Restaurant::all();
+        $user_id = auth()->id();
+        $restaurants = Restaurant::where('user_id', $user_id)->get();
         return response()->json($restaurants);
     }
 
@@ -24,34 +24,33 @@ class RestaurantController extends Controller
      */
     public function store(Request $request)
     {
-        $validator = Validator::make($request->all(), [
+        $user_id = 1;
+        
+        $validated = $request -> validate([
             'nom' => 'required|string|max:255',
             'adresse' => 'required|string|max:255',
-            'horaires' => 'required|string|max:255',
-            'image' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'horaires_ouverture' => 'required|string|max:255',
+            'image_illustration' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg,webp|max:2048',
         ]);
 
-        if ($validator->fails()) {
-            return response()->json($validator->errors(), 400);
+        $restaurant = new Restaurant($request->only(['nom', 'adresse', 'horaires_ouverture']));
+        $restaurant->user_id = $user_id;
+        if ($request->hasFile('image_illustration')) {
+            $path = $request->file('image_illustration')->store('public/restaurants');
+            $restaurant->image_illustration = str_replace('public/', 'storage/', $path);
         }
 
-        $restaurant = new Restaurant($request->only(['nom', 'adresse', 'horaires']));
-        if ($request->hasFile('image')) {
-            $path = $request->file('image')->store('public/restaurants');
-            $restaurant->image = $path;
-        }
         $restaurant->save();
-
+        
         return response()->json(['message' => 'Restaurant créé avec succès.', 'restaurant' => $restaurant], 201);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Restaurant $restaurant)
     {
-        // récupérer un restaurant spécifique par ID
-        $restaurant = Restaurant::find($id);
+        $restaurant = Restaurant::find($restaurant);
         if ($restaurant) {
             return response()->json($restaurant);
         } else {
@@ -62,30 +61,29 @@ class RestaurantController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Restaurant $restaurant)
     {
-        // mettre à jour un restaurant spécifique
         $validator = Validator::make($request->all(), [
-            'nom' => 'sometimes|required|string|max:255',    // 'sometimes' signifie que la validation suivante ne sera effectuée que si 'nom' est présent dans la requête.
+            'nom' => 'sometimes|required|string|max:255',   
             'adresse' => 'sometimes|required|string|max:255',
-            'horaires' => 'sometimes|required|string|max:255',
-            'image' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'horaires_ouverture' => 'sometimes|required|string|max:255',
+            'image_illustration' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg,webp|max:2048',
         ]);
 
         if ($validator->fails()) {
             return response()->json($validator->errors(), 400);
         }
 
-        $restaurant = Restaurant::find($id);
+        $restaurant = Restaurant::find($restaurant);
         if (!$restaurant) {
             return response()->json(['message' => 'Restaurant non trouvé'], 404);
         }
 
         $restaurant->update($request->only(['nom', 'adresse', 'horaires']));
 
-        if ($request->hasFile('image')) {
-            $path = $request->file('image')->store('public/restaurants');
-            $restaurant->image = $path;
+        if ($request->hasFile('image_illustration')) {
+            $path = $request->file('image_illustration')->store('public/restaurants');
+            $restaurant->image_illustration = str_replace('public/', 'storage/', $path);
         }
 
         $restaurant->save();
@@ -95,10 +93,9 @@ class RestaurantController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Restaurant $restaurant)
     {
-        // supprimer un restaurant spécifique
-        $restaurant = Restaurant::find($id);
+        $restaurant = Restaurant::find($restaurant);
         if ($restaurant) {
             $restaurant->delete();
             return response()->json(['message' => 'Restaurant supprimé avec succès']);
--- routes/api.php
+++ routes/api.php
@@ -2,25 +2,16 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\RestaurantController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
 
-use App\Http\Controllers\Api\RestaurantController;
 
-// Route pour obtenir tous les restaurants
-Route::get('/restaurants', [RestaurantController::class, 'index']);
-
-// Route pour créer un nouveau restaurant
-Route::post('/restaurants', [RestaurantController::class, 'store']);
-
-// Route pour afficher un restaurant spécifique
-Route::get('/restaurants/{id}', [RestaurantController::class, 'show']);
-
-// Route pour mettre à jour un restaurant
-Route::put('/restaurants/{id}', [RestaurantController::class, 'update']);
-
-// Route pour supprimer un restaurant
-Route::delete('/restaurants/{id}', [RestaurantController::class, 'destroy']);
+Route::get('/restaurants', [RestaurantController::class, 'index'])->name('restaurants.index');
+Route::post('/restaurants', [RestaurantController::class, 'store'])->name('restaurants.store');
+Route::get('/restaurants/{restaurant}', [RestaurantController::class, 'show'])->name('restaurants.show');
+Route::put('/restaurants/{restaurant}', [RestaurantController::class, 'update'])->name('restaurants.update');
+Route::delete('/restaurants/{restaurant}', [RestaurantController::class, 'destroy'])->name('restaurants.destroy');
