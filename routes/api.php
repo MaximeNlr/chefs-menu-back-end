3c2f6bd20c57f5762e0dbefd4597a11df250abf4
nicolasJiv <nicolas.jivan@gmail.com>
2024-04-23 10:19:15 +0200

    test qrcode

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\RestaurantController;
+use App\Http\Controllers\Api\QrCodeController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -15,3 +16,6 @@ Route::post('/restaurants', [RestaurantController::class, 'store'])->name('resta
 Route::get('/restaurants/{restaurant}', [RestaurantController::class, 'show'])->name('restaurants.show');
 Route::put('/restaurants/{restaurant}', [RestaurantController::class, 'update'])->name('restaurants.update');
 Route::delete('/restaurants/{restaurant}', [RestaurantController::class, 'destroy'])->name('restaurants.destroy');
+
+Route::post('/generate-qrcode', [QrCodeController::class, 'generate']);
+
