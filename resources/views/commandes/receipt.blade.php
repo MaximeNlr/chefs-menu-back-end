ad304a485abf64c0c73f9758e8c27c99a51680b9
LucasDWWM <lucaslipari06@gmail.com>
2024-04-26 15:23:26 +0200

    Affichage des commandes

--- resources/views/commandes/receipt.blade.php
+++ resources/views/commandes/receipt.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Receipt</title>
+    <title>Récapitulatif</title>
 </head>
 <body>
     <h1>Récapitulatif de la commande {{ $commande->id }}</h1>
